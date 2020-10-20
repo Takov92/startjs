117f6e4afb707e5c073f95639ff47b3df72a0fd5
illia <jhongolt688@gmail.com>
2020-10-20 18:39:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// types of date
+// let number=5;
+// const person='alex';
+// const bool=true;
+
+// const obj={
+//     name:'Jhon',
+//     age:25,
+//     isMarried:false
+// };
+
+// console.log(obj ['name']);
+ 
+//  let arr=['plump.png,', 26, 'jhon', {obj}];
+// console.log(arr);
+
+
+// простое общение
+// alert('Hello');
+
+
+// const result= confirm('Are you here');
+// console.log(result);
+
+// const answer=prompt('Вам есть 18', '');
+// console.log(answer +5);
+// // результат будет anwer и плюспять
+
+// const answer=+prompt('Вам есть 18', '');
+// console.log(answer +5);
+// // отработает сложение
+
+
+// Операторы 
+console.log('arr'+"-object");
+
+// incriment dicriment
+let incr=10;
+let decr=10;
+
+// ++incr; 
+// --decr;
+
+console.log(incr++);
+ 
+console.log(decr--);
+
+
+
