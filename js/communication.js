9393d1708d9a5f2b776abf8b3c3498a04d7a0eda
illia <jhongolt688@gmail.com>
2020-11-06 20:02:23 +0200

    new project

---
+++ js/communication.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+// 
+
+// выводит окно с данными и предложением "ок" "отмена"
+// дынные выводятся в переменную 
+// const result=confirm ('Are you here'); 
+// console.log(result);
+
+
+// // Выводит пользователю указанные данные с возможностью 
+// // вписать ответ.Сам ответ помещается в переменную
+// // В последние кавычки можно вписать предполагаемый ответ 
+// const answer=prompt('How are you',''); 
+// console.log(answer);
+
+// const answer=prompt('How are you',''); 
+// console.log(answer + 5); Выведет ответ склеянный с пятеркой 
+
+// const answer=+prompt('How are you',''); 
+// console.log(answer + 5);Выведет ответ-результат сложения
+// // typeof=проверяет тип дынных
+
+// const answer=[];
+
+// answer[0]=prompt('как ваше имя?','');
+// answer[1]=prompt('как ваша фамилия?','');
+// answer[2]=prompt('Сколько вам лет?','');
+
+// document.write(answer);
+
+// console.log(typeof(answer)); 
+
+
+// Интерполяция
+
+// const category='toys';
+// // console.log('https://someurl.com/'+category);
+// console.log(`https://someurl.com/${category}/5`);
+
+const user='illia';
+
+alert(`Привет, ${user}`);
---
+++ js/start.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+let number=5;
+let myName='illia';
+const leftBorderWidth=1;
+let und;
+
+console.log(number+myName);
+
+
+// Простые типы данных 
+
+// порстые типы
+
+// число
+// строка
+// Логический тип
+// Null- когда не существует 
+// undefined- когда существует, но нет значения
+// symbol-
+// Bigint- большое число 
+
+// console.log('string'*9); Выводит Nan
+
+// console.log(-4/0); Выводит -Infinity
+
+// console.log(ter); Выводит ошибку
+
+// console.log(und);
+// Объекты
+
+// Массивы
+// Функци
+// Объект даты
+// Регуляргые выражния
+// Ошибки
+
+
+const obj={
+    name:'Jhon',
+    age:25,
+    isMarrid:false
+
+};
+
+console.log(obj.name);выводит все правильно 
+console.log(obj[name]); выводит ошибку
+console.log(obj['name']); выводит все правильно 
+
+let arr=['plump.png', 'orange.jpg', 6 ,'apple.bmp', {}, []];
+console.log(arr[1]);
+
+// Отличие массива от объекта в том, что массив
+// имеет значния которые расположено по порядку,а
+// объект ккаждому значению присваивает ключ
+
+
